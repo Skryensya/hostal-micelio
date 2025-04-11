2d4bc6836794190c4280d7ab48d50c53be02d53f
Allison <Allison.jpb@gmail.com>
2025-04-11 00:03:46 -0400

    git fix build

--- src/components/ImagesShowcaseGrid.tsx
+++ src/components/ImagesShowcaseGrid.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import Image from "next/image";
-import { cn } from "@/lib/utils";
+import Image from "next/image"; 
 import { useGallery } from "./GalleryProvider";
 
 export type ImageType = {
